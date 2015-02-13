d9c8c0ec023549e037fd95a48c39c522aa89932b
unknown <webfans@yeah.net>
2015-02-14 00:57:43 +0800

    Signed-off-by: unknown <webfans@yeah.net>

---
+++ trunk/SelfHost/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using System.Web.Http.SelfHost;
+
+namespace SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assembly wbAPI = Assembly.Load("WebApi, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+
+            //Console.WriteLine(wbAPI.CodeBase);
+
+            //HttpSelfHostConfiguration configuration = new HttpSelfHostConfiguration("http://localhost/selfhost");
+
+            //using (HttpSelfHostServer httpServer = new HttpSelfHostServer(configuration))
+            //{
+            //    httpServer.Configuration.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional });
+
+            //    httpServer.OpenAsync();
+            //    Console.Read();
+            //}
+
+            HttpSelfHostConfiguration configuration = new HttpSelfHostConfiguration("http://localhost/selfhost");
+            using (HttpSelfHostServer httpServer = new HttpSelfHostServer(configuration))
+            {
+                httpServer.Configuration.Services.Replace(typeof(IAssembliesResolver), new ExtendedDefaultAssembliesResolver());
+                //其他操作
+                httpServer.Configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+                httpServer.OpenAsync();
+                Console.Read();
+            }
+
+            //Assembly.Load("WebApi, Version=1.0.0.0, Culture=neutral,  PublicKeyToken=null");
+            //HttpSelfHostConfiguration configuration = new HttpSelfHostConfiguration("http://localhost/selfhost");
+            //using (HttpSelfHostServer httpServer = new HttpSelfHostServer(configuration))
+            //{
+            //    httpServer.Configuration.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional });
+
+            //    httpServer.OpenAsync();
+
+            //    Console.Read();
+            //}
+        }
+    }
+}
---
+++ trunk/WebApi/ContactsController.cs
@@ -0,0 +1,61 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi
+{
+    public class ContactsController : ApiController
+    {
+        static List<Contact> contacts;
+        static ContactsController()
+        {
+            contacts = new List<Contact>();
+
+            contacts.Add(new Contact
+            {
+                Id = "001",
+                Name = "张三",
+                PhoneNo = "0512-12345678",
+                EmailAddress = "zhanqsan@gmail.com",
+                Address = "江苏省苏州市星湖街328号"
+            });
+
+            contacts.Add(new Contact
+            {
+                Id = "002",
+                Name = "李四",
+                PhoneNo = "0512-23迢56789",
+                EmailAddress = "lis@gmail.com",
+                Address = "江苏省苏州市金鸡湖欠道328号"
+            });
+        }
+
+        public IEnumerable<Contact> Get(string id = null)
+        {
+            return from contact in contacts
+                   where contact.Id == id || string.IsNullOrEmpty(id)
+                   select contact;
+        }
+
+        public void Post(Contact contact)
+        {
+            contact.Id = Convert.ToString("D3");
+            contacts.Add(contact);
+        }
+
+        public void Put(Contact contact)
+        {
+            contacts.Remove(contacts.First(c => c.Id == contact.Id));
+            contacts.Add(contact);
+        }
+
+        public void Delete(string Id)
+        {
+            contacts.Remove(contacts.First(c => c.Id == Id));
+        }
+    }
+}
